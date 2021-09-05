84bb285feeaedd690393f562b6a6baba61f2de15
vinay <vinaygupta7516@gmail.com>
2021-09-05 19:51:48 +0530

    first

---
+++ README.md
@@ -0,0 +1,12 @@
+# This created on Svelte
+
+**_created by Vinay_**
+
+```javascript
+    Go to project folder
+    // to install dependencies
+    npm install
+    // to run project
+    npm run dev
+    // now go to http://localhost:5000/
+```
---
+++ src/App.svelte
@@ -0,0 +1,29 @@
+<script>
+	let count=0
+	const Increment =()=>{
+	count=count+1
+	}
+	const Decrement =()=>{
+	if(count<1){
+		alert("Count can't negative")
+	}else{count=count-1}
+	}
+	$: if (count >= 10) {
+	alert(`count is dangerously high!`);
+	count = 9;
+}
+</script>
+<div class="container">
+	<h1>Click Counter</h1>
+	<p>count is {count}</p>
+	<button on:click={Increment}>Increase</button>
+	<button on:click={Decrement}>Decrease</button>
+</div>
+<style>
+.container{
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+}
+</style>
